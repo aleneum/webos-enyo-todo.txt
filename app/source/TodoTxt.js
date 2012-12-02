49c9b6e14fe33290d7c1d5e1f0f69acf08269219
Morgan McMillian <gilag@monkeystew.com>
2012-12-02 07:16:45 -0500

    fix duplication when retrieving done file

--- app/source/TodoTxt.js
+++ app/source/TodoTxt.js
@@ -411,6 +411,7 @@ enyo.kind({
 
     loadArchive: function(path, file) {
         // TODO stub
+        this.doneList = [];
         var list = file.content.split("\n");
         for (item in list) {
             if (list[item].length > 0) {
